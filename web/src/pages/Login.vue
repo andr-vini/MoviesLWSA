a4bb3cb3f891527dcd77f66da0b2fba2368d5bba
André Vinicius <andrevinicius.dev@gmail.com>
2025-05-30 10:54:30 -0300

    fix: removendo classe tailwind inexistente

--- web/src/pages/Login.vue
+++ web/src/pages/Login.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-    <div class="sm:max-w-80/100 mx-auto">
+    <div class="mx-auto">
         <header class="min-h-10 border-b-gray-200 border-b py-5">
             <h2 class="text-center sm:text-left text-2xl text-green-600 font-semibold">Movies</h2>
         </header>
